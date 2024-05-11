6351b2030350ae9888535c9dacc6cfbfb325142a
ovaserashid <159573241+Ovaserashid@users.noreply.github.com>
2024-05-11 21:46:19 +0530

    create getComputerChoice function
    Created the function to get the computer's choice, the function
    returns rock, paper and scissor as intended.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("hello");
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random()*3);
+    let compChoice = "";
+    if(randomNum === 0){
+        compChoice = "rock";
+    }else if(randomNum === 1){
+        compChoice = "paper";
+    }else {
+        compChoice = "scissor";
+    }
+    return compChoice;
+}
+
+console.log(getComputerChoice());
