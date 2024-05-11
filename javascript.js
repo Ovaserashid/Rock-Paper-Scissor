3628a5f8d6dcb339d268b83f2818cf4188863b06
ovaserashid <159573241+Ovaserashid@users.noreply.github.com>
2024-05-11 22:22:16 +0530

    create playRound function with all logic
    Created the function with two parameters one human choice and the
    other for computer choice. First checked if the values are the
    same then alerted same choice and then checked all the possible
    combinations and inside each combination declared winner or loser.
    Also incremented the score of winner.

--- javascript.js
+++ javascript.js
@@ -20,4 +20,29 @@ function getHumanChoice(){
         return userInput;
     }else return "Invalid Choice";
 }
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        alert("Same response by both players!! try again!");
+    }else if(humanChoice === "rock" && computerChoice === "paper"){
+        computerScore ++;
+        alert("You lose! Paper beats Rock");
+    }else if(humanChoice === "rock" && computerChoice === "scissor"){
+        humanScore++;
+        alert("You win! Rock beats Scissor");
+    }else if(humanChoice === "paper" && computerChoice === "rock"){
+        humanScore++;
+        alert("You win! Paper beats Rock");
+    }else if(humanChoice === "paper" && computerChoice === "scissor"){
+        computerScore++;
+        alert("You Lose! Scissor beats Paper");
+    }else if(humanChoice === "scissor" && computerChoice === "paper"){
+        humanScore++;
+        alert("You win! Scissor beats Paper");
+    }else if(humanChoice === "scissor" && computerChoice === "rock"){
+        computerScore++;
+        alert("You Lose! Rock beats Scissor");
+    }else alert("Invalid Choice");
+}
+playRound(getHumanChoice(),getComputerChoice());
